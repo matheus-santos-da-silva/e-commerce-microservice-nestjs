894a217b89b3a7fe826ea96600d1cc13d8d37b11
Matheus Santos <maatheus.santos223@gmail.com>
2024-03-18 14:42:57 -0300

    fet: add create-user-controller docs

--- client-service/src/presentation/controllers/create-user-controller.ts
+++ client-service/src/presentation/controllers/create-user-controller.ts
@@ -1,14 +1,25 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { CreateUserDTO } from 'src/domain/DTOS/create-user-dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreateUser } from 'src/domain/use-cases/';
+import { CreateUserVM, UserResponseViewModel } from '../view-models';
+import { HttpBadRequestError, HttpConflictError } from '../swagger/http-errors';
 
+@ApiTags('Users')
 @Controller('users')
 export class CreateUserController {
   constructor(private readonly createUser: CreateUser) {}
 
+  @ApiOperation({ summary: 'Create User' })
+  @ApiResponse({
+    status: 201,
+    description: 'Created',
+    type: UserResponseViewModel,
+  })
+  @ApiResponse(HttpBadRequestError)
+  @ApiResponse(HttpConflictError)
   @Post('create')
-  async execute(@Body() user: CreateUserDTO) {
+  async execute(@Body() user: CreateUserVM): Promise<UserResponseViewModel> {
     const newUser = await this.createUser.create(user);
-    return newUser;
+    return UserResponseViewModel.toViewModel(newUser);
   }
 }
