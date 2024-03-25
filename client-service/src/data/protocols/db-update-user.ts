8e2b6a979b2ee62e11bcc96b7284e778ba083c52
Matheus Santos <maatheus.santos223@gmail.com>
2024-03-25 14:03:29 -0300

    fix: change email and phone validation in update-user-implementation

--- client-service/src/data/protocols/db-update-user.ts
+++ client-service/src/data/protocols/db-update-user.ts
@@ -3,5 +3,4 @@ import { User } from 'src/domain/models/user';
 
 export abstract class UpdateUserRepository {
   abstract update(id: string, user: UpdateUserDTO): Promise<User>;
-  abstract findUserById(id: string): Promise<User>;
 }
--- client-service/src/data/use-cases-implementation/update-user-impl.ts
+++ client-service/src/data/use-cases-implementation/update-user-impl.ts
@@ -1,16 +1,32 @@
 import { User } from 'src/domain/models/user';
 import { UpdateUser } from 'src/domain/use-cases';
-import { UpdateUserRepository } from '../protocols';
+import { GetUserByFieldsRepository, UpdateUserRepository } from '../protocols';
 import { UpdateUserDTO } from 'src/domain/DTOS/update-user-dto';
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { encryptingPass } from '../utils/encrypt-pass';
 
 @Injectable()
 export class UpdateUserImplementation implements UpdateUser {
-  constructor(private readonly repository: UpdateUserRepository) {}
+  constructor(
+    private readonly repository: UpdateUserRepository,
+    private readonly getUserByfields: GetUserByFieldsRepository,
+  ) {}
 
   async update(id: string, user: UpdateUserDTO): Promise<User> {
-    await this.repository.findUserById(id);
+    if (user.email || user.phone) {
+      const checkUserEmail = await this.getUserByfields.getUserByEmail(
+        user.email,
+      );
+      const checkUserPhone = await this.getUserByfields.getUserByPhone(
+        user.phone,
+      );
+      if (checkUserEmail) {
+        throw new ConflictException('Email is already in use');
+      }
+      if (checkUserPhone) {
+        throw new ConflictException('Phone is already in use');
+      }
+    }
 
     if (user.password) {
       const passwordHash = await encryptingPass(user.password);
