792ca8a1fe97d7130f86ee52534d99c89b8f64aa
Matheus Santos <maatheus.santos223@gmail.com>
2024-03-25 14:04:52 -0300

    feat: add jwt validation in delete-user and update-user use-cases

--- client-service/src/presentation/controllers/delete-user-controller.ts
+++ client-service/src/presentation/controllers/delete-user-controller.ts
@@ -1,12 +1,31 @@
-import { Controller, Delete, Param } from '@nestjs/common';
-import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { DeleteUser } from 'src/domain/use-cases/';
-import { HttpNotFoundError } from '../swagger/http-errors';
+import {
+  Controller,
+  Delete,
+  NotFoundException,
+  Param,
+  Headers,
+} from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { DeleteUser, GetUserById } from 'src/domain/use-cases/';
+import {
+  HttpNotFoundError,
+  HttpUnauthorizedError,
+} from '../swagger/http-errors';
+import { ValidateToken } from 'src/data/utils/validate-token';
 
 @ApiTags('Users')
 @Controller('users')
 export class DeleteUserController {
-  constructor(private readonly deleteUser: DeleteUser) {}
+  constructor(
+    private readonly deleteUser: DeleteUser,
+    private readonly getUserById: GetUserById,
+  ) {}
 
   @ApiOperation({ summary: 'Delete User' })
   @ApiResponse({
@@ -14,9 +33,18 @@ export class DeleteUserController {
     description: 'Success',
   })
   @ApiResponse(HttpNotFoundError)
+  @ApiResponse(HttpUnauthorizedError)
   @ApiParam({ name: 'id', type: String })
+  @ApiBearerAuth()
   @Delete(':id')
-  async execute(@Param('id') id: string): Promise<void> {
+  async execute(
+    @Param('id') id: string,
+    @Headers('Authorization') authorization: string,
+  ): Promise<void> {
+    await ValidateToken(authorization, id);
+
+    const checkUser = await this.getUserById.getById(id);
+    if (!checkUser) throw new NotFoundException('User was not found');
     await this.deleteUser.delete(id);
   }
 }
--- client-service/src/presentation/controllers/update-user-controller.ts
+++ client-service/src/presentation/controllers/update-user-controller.ts
@@ -1,13 +1,34 @@
-import { Body, Controller, Param, Patch } from '@nestjs/common';
-import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { UpdateUser } from 'src/domain/use-cases/';
+import {
+  Body,
+  Controller,
+  NotFoundException,
+  Param,
+  Patch,
+  Headers,
+} from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { UpdateUser, GetUserById } from 'src/domain/use-cases/';
 import { UpdateUserVM, UserResponseViewModel } from '../view-models';
-import { HttpNotFoundError } from '../swagger/http-errors';
+import {
+  HttpConflictError,
+  HttpNotFoundError,
+  HttpUnauthorizedError,
+} from '../swagger/http-errors';
+import { ValidateToken } from 'src/data/utils/validate-token';
 
 @ApiTags('Users')
 @Controller('users')
 export class UpdateUserController {
-  constructor(private readonly updateUser: UpdateUser) {}
+  constructor(
+    private readonly updateUser: UpdateUser,
+    private readonly getUserById: GetUserById,
+  ) {}
 
   @ApiOperation({ summary: 'Update User' })
   @ApiResponse({
@@ -16,14 +37,21 @@ export class UpdateUserController {
     type: UserResponseViewModel,
   })
   @ApiResponse(HttpNotFoundError)
+  @ApiResponse(HttpUnauthorizedError)
+  @ApiResponse(HttpConflictError)
   @ApiParam({ name: 'id', type: String })
+  @ApiBearerAuth()
   @Patch(':id')
   async execute(
     @Param('id') id: string,
     @Body() user: UpdateUserVM,
+    @Headers('Authorization') authorization: string,
   ): Promise<UserResponseViewModel> {
+    await ValidateToken(authorization, id);
+    const checkUser = await this.getUserById.getById(id);
+    if (!checkUser) throw new NotFoundException('User was not found');
     const updatedUser = await this.updateUser.update(id, user);
 
-    return updatedUser;
+    return UserResponseViewModel.toViewModel(updatedUser);
   }
 }
