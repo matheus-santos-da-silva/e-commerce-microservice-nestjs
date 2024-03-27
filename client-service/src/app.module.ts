b1ac14cc36369b6c52f2cc13554b27274546e83f
Matheus Santos <maatheus.santos223@gmail.com>
2024-03-27 14:16:05 -0300

    feat: add redis-config pub-sub-service and module

--- client-service/src/app.module.ts
+++ client-service/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersControllersModule } from './presentation/controllers/users-controllers.module';
+import { ConfigModule } from '@nestjs/config';
+import { PubSubModule } from './infra/messaging/redis/pubsub.module';
 
 @Module({
-  imports: [UsersControllersModule],
+  imports: [UsersControllersModule, ConfigModule.forRoot(), PubSubModule],
   controllers: [],
   providers: [],
 })
--- client-service/src/main.ts
+++ client-service/src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { addRedisClient } from './infra/messaging/redis/config/redis.config';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get<ConfigService>(ConfigService);
 
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
@@ -19,5 +22,9 @@ async function bootstrap() {
   SwaggerModule.setup('api-docs', app, document);
 
   await app.listen(`${process.env.PORT}`);
+  await addRedisClient(configService).catch((error) => {
+    console.error('Failed to initialize Redis client:', error);
+    process.exit(1);
+  });
 }
 bootstrap();
