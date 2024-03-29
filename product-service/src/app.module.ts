c684fe60b6bce5bb89651a626886176ce746ada7
Matheus Santos <maatheus.santos223@gmail.com>
2024-03-29 16:02:25 -0300

    feat: add configs in app-module and main

--- product-service/src/app.module.ts
+++ product-service/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ProductsControllersModule } from './presentation/controllers/products/products-controllers.module';
 
 @Module({
-  imports: [],
+  imports: [ProductsControllersModule],
   controllers: [],
   providers: [],
 })
--- product-service/src/main.ts
+++ product-service/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { PORT } from './env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle('Product-Service')
+    .setDescription('Product Microservice of E-commerce')
+    .setVersion('1')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  await app.listen(PORT);
 }
 bootstrap();
