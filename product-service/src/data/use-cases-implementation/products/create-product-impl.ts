aab72f8b5a09c6399671f1e7b302f1d634975f57
Matheus Santos <maatheus.santos223@gmail.com>
2024-04-04 14:21:14 -0300

    feat: sending product to create-product topic in create-product use-case

--- product-service/src/data/use-cases-implementation/products/create-product-impl.ts
+++ product-service/src/data/use-cases-implementation/products/create-product-impl.ts
@@ -3,13 +3,26 @@ import { CreateProductRepository } from 'src/data/protocols/products/db-create-p
 import { CreateProductDTO } from 'src/domain/DTOS/create-product-dto';
 import { Product } from 'src/domain/models/products/product';
 import { CreateProduct } from 'src/domain/use-cases/products';
+import { PubSubService } from 'src/infra/messaging/redis/pubsub.service';
 
 @Injectable()
 export class CreateProductImplementation implements CreateProduct {
-  constructor(private readonly repository: CreateProductRepository) {}
+  constructor(
+    private readonly repository: CreateProductRepository,
+    private readonly messagingService: PubSubService,
+  ) {}
 
   async create(product: CreateProductDTO): Promise<Product> {
     const newProduct = await this.repository.create(product);
+    await this.messagingService.publish(
+      'create-product',
+      {
+        name: product.name,
+        code: product.code,
+        price: product.price,
+      },
+      'products',
+    );
     return newProduct;
   }
 }
