2e66325dd171519f2a2d8eff217504214a55ec65
Matheus Santos <maatheus.santos223@gmail.com>
2024-04-04 14:43:51 -0300

    feat: sending message to delete-user topic with the user-id that was deleted

--- client-service/src/data/use-cases-implementation/delete-user-impl.ts
+++ client-service/src/data/use-cases-implementation/delete-user-impl.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { DeleteUser } from 'src/domain/use-cases';
 import { DeleteUserRepository } from '../protocols';
+import { PubSubService } from 'src/infra/messaging/redis/pubsub.service';
 
 @Injectable()
 export class DeleteUserImplementation implements DeleteUser {
-  constructor(private readonly repository: DeleteUserRepository) {}
+  constructor(
+    private readonly repository: DeleteUserRepository,
+    private readonly messagingService: PubSubService,
+  ) {}
 
   async delete(id: string): Promise<void> {
     await this.repository.delete(id);
+    await this.messagingService.publish('delete-user', { id }, 'users');
   }
 }
