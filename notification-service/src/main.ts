e7d69d1c9e2f34a4f756be70c36b8ac99959fb81
Matheus Santos <maatheus.santos223@gmail.com>
2024-04-29 12:45:29 -0300

    feat: add redis config

--- notification-service/src/main.ts
+++ notification-service/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PORT } from './env';
+import { ConfigService } from '@nestjs/config';
+import { addRedisClient } from './infra/messaging/redis/config/redis.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get<ConfigService>(ConfigService);
+
+  await app.listen(PORT);
+  await addRedisClient(configService).catch((error) => {
+    console.error('Failed to initialize Redis client:', error);
+    process.exit(1);
+  });
 }
 bootstrap();
